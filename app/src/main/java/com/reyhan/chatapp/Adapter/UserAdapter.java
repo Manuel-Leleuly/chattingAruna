8282583be3a9f1984d11bf85c6347ea0efb7a90e
Reyhan <miyabruno21@gmail.com>
2019-07-09 10:24:11 +0700

    Display user from database

---
+++ app/src/main/java/com/reyhan/chatapp/Adapter/UserAdapter.java
@@ -0,0 +1,68 @@
+package com.reyhan.chatapp.Adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.reyhan.chatapp.Model.User;
+import com.reyhan.chatapp.R;
+
+import java.util.List;
+
+//buat adapter untuk menampilkan user
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
+
+    private Context context;
+    private List<User> users;
+
+    public UserAdapter(Context context, List<User> users) {
+        this.context = context;
+        this.users = users;
+    }
+
+    //tampilan akan berdasar pada layout user yang dibuat (binding)
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.user, viewGroup, false);
+        return new UserAdapter.ViewHolder(view);
+    }
+
+    //tarik data dari db dan set dilayout
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        User user = users.get(i);
+        viewHolder.username.setText(user.getUsername());
+        if (user.getImageURL().equals("default")){
+            viewHolder.profile.setImageResource(R.drawable.user);
+        } else {
+            Glide.with(context).load(user.getImageURL()).into(viewHolder.profile);
+        }
+    }
+
+    //memunculkan user sebanyak data didatabase
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView username;
+        public ImageView profile;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            username = itemView.findViewById(R.id.usertextview);
+            profile = itemView.findViewById(R.id.userimage);
+
+        }
+    }
+}
--- app/src/main/java/com/reyhan/chatapp/Fragment/ContactFragment.java
+++ app/src/main/java/com/reyhan/chatapp/Fragment/ContactFragment.java
@@ -2,29 +2,86 @@ package com.reyhan.chatapp.Fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.reyhan.chatapp.Adapter.UserAdapter;
+import com.reyhan.chatapp.Model.User;
 import com.reyhan.chatapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class ContactFragment extends Fragment {
 
+    //panggil fungsi adapter dan list untuk diimplementasikan ke fragment
+
+    private RecyclerView recyclerView;
+    private UserAdapter userAdapter;
+    private List<User> users;
 
     public ContactFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_contact, container,false);
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        users = new ArrayList<>();
+
+        //buat fungsi membaca user
+        readUsers();
+
+        return view;
+    }
+
+    private void readUsers() {
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                users.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    User user = snapshot.getValue(User.class);
+
+                    assert user != null;
+                    assert firebaseUser != null;
+                    if (!user.getId().equals(firebaseUser.getUid())){
+                        users.add(user);
+                    }
+                }
+
+                userAdapter = new UserAdapter(getContext(), users);
+                recyclerView.setAdapter(userAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
