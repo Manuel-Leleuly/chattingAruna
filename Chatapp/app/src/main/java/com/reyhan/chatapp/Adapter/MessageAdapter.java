bba45aa8532deb5e9a5bc38771313738705a2ac0
manuel-leleuly <manuel.leleuly@gmail.com>
2019-07-10 14:16:20 +0700

    tau ah 2

---
+++ Chatapp/app/src/main/java/com/reyhan/chatapp/Adapter/MessageAdapter.java
@@ -0,0 +1,98 @@
+package com.reyhan.chatapp.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.reyhan.chatapp.MessageActivity;
+import com.reyhan.chatapp.Model.Chat;
+import com.reyhan.chatapp.Model.User;
+import com.reyhan.chatapp.R;
+
+import java.util.List;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHolder>{
+
+    public static final int MSG_TYPE_LEFT = 0;
+    public static final int MSG_TYPE_RIGHT = 1;
+
+    private Context context;
+    private List<Chat> chat;
+    private String imageurl;
+
+    FirebaseUser firebaseUser;
+
+    public MessageAdapter(Context context, List<Chat> chat, String imageurl) {
+        this.context = context;
+        this.chat = chat;
+        this.imageurl = imageurl;
+    }
+
+    //tampilan akan berdasar pada layout user yang dibuat (binding)
+    @NonNull
+    @Override
+    public MessageAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        if(i == MSG_TYPE_RIGHT) {
+            View view = LayoutInflater.from(context).inflate(R.layout.user, viewGroup, false);
+            return new MessageAdapter.ViewHolder(view);
+        }
+        else{
+            View view = LayoutInflater.from(context).inflate(R.layout.chat_item_left, viewGroup,false);
+            return new MessageAdapter.ViewHolder(view);
+        }
+    }
+
+    //tarik data dari db dan set dilayout
+    @Override
+    public void onBindViewHolder(@NonNull MessageAdapter.ViewHolder viewHolder, int i) {
+        Chat pesan = chat.get(i);
+        viewHolder.show_message.setText(pesan.getMessage());
+        if(imageurl.equals("default")){
+            viewHolder.profile.setImageResource(R.drawable.user);
+        }
+        else{
+            Glide.with(context).load(imageurl).into(viewHolder.profile);
+        }
+    }
+
+    //memunculkan user sebanyak data didatabase
+    @Override
+    public int getItemCount() {
+        return chat.size();
+    }
+
+    //binding data di db dan di layout
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView show_message;
+        private ImageView profile;
+
+        private ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            show_message = itemView.findViewById(R.id.show_message);
+            profile = itemView.findViewById(R.id.userimage);
+
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(chat.get(position).getSender().equals(firebaseUser.getUid())){
+            return MSG_TYPE_RIGHT;
+        }
+        else{
+            return MSG_TYPE_LEFT;
+        }
+    }
+}
--- Chatapp/app/src/main/java/com/reyhan/chatapp/MessageActivity.java
+++ Chatapp/app/src/main/java/com/reyhan/chatapp/MessageActivity.java
@@ -4,9 +4,14 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.bumptech.glide.Glide;
@@ -17,7 +22,13 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.reyhan.chatapp.Adapter.MessageAdapter;
+import com.reyhan.chatapp.Model.Chat;
 import com.reyhan.chatapp.Model.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -28,6 +39,14 @@ public class MessageActivity extends AppCompatActivity {
 
     FirebaseUser firebaseUser;
     DatabaseReference reference;
+
+    ImageButton btn_send;
+    EditText text_send;
+
+    MessageAdapter messageAdapter;
+    List<Chat> chat;
+
+    RecyclerView recyclerView;
 
     Intent intent;
 
@@ -48,15 +67,37 @@ public class MessageActivity extends AppCompatActivity {
             }
         });
 
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
         //binding
         circleImageView = findViewById(R.id.profil_image);
         username = findViewById(R.id.usernamecontact);
+        btn_send = findViewById(R.id.btn_send);
+        text_send = findViewById(R.id.text_send);
 
         //ambil id user dari db
         intent = getIntent();
         final String userid = intent.getStringExtra("userid");
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String msg = text_send.getText().toString();
+                if(!msg.equals("")){
+                    sendMessage(firebaseUser.getUid(), userid, msg);
+                }
+                else{
+                    Toast.makeText(MessageActivity.this, "Tidak bisa mengirim pesan kosong", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
 
         reference.addValueEventListener(new ValueEventListener() {
@@ -68,6 +109,43 @@ public class MessageActivity extends AppCompatActivity {
                     circleImageView.setImageResource(R.drawable.user);
                 } else {
                     Glide.with(MessageActivity.this).load(user.getImageURL()).into(circleImageView);
+                }
+
+                readMessages(firebaseUser.getUid(), userid, user.getImageURL());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void sendMessage(String sender, String receiver, String message){
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("sender", sender);
+        hashMap.put("receiver", receiver);
+        hashMap.put("message", message);
+
+        reference.child("Chats").push().setValue(hashMap);
+    }
+
+    private void readMessages(final String myid, final String userid, final String imageurl){
+        chat = new ArrayList<>();
+
+        reference = FirebaseDatabase.getInstance().getReference("Chats");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                chat.clear();
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Chat pesan = snapshot.getValue(Chat.class);
+                    if(pesan.getReceiver().equals(userid) && pesan.getSender().equals(userid) || pesan.getReceiver().equals(userid) && pesan.getSender().equals(userid)){
+                        chat.add(pesan);
+                    }
+
+                    messageAdapter = new MessageAdapter(MessageActivity.this,chat,imageurl);
+                    recyclerView.setAdapter(messageAdapter);
                 }
             }
 
