69c80187fb7891f54df70a731a097010d9493490
manuel-leleuly <manuel.leleuly@gmail.com>
2019-07-10 13:10:54 +0700

    tau ah

--- Chatapp/app/src/main/java/com/reyhan/chatapp/MessageActivity.java
+++ Chatapp/app/src/main/java/com/reyhan/chatapp/MessageActivity.java
@@ -6,10 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 import com.bumptech.glide.Glide;
@@ -22,8 +19,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.reyhan.chatapp.Model.User;
 
-import java.util.HashMap;
-
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MessageActivity extends AppCompatActivity {
@@ -33,9 +28,6 @@ public class MessageActivity extends AppCompatActivity {
 
     FirebaseUser firebaseUser;
     DatabaseReference reference;
-
-    ImageButton btn_send;
-    EditText text_send;
 
     Intent intent;
 
@@ -59,28 +51,12 @@ public class MessageActivity extends AppCompatActivity {
         //binding
         circleImageView = findViewById(R.id.profil_image);
         username = findViewById(R.id.usernamecontact);
-        btn_send = findViewById(R.id.btn_send);
-        text_send = findViewById(R.id.text_send);
 
         //ambil id user dari db
         intent = getIntent();
         final String userid = intent.getStringExtra("userid");
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-
-        btn_send.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String msg = text_send.getText().toString();
-                if(!msg.equals("")){
-                    sendMessage(firebaseUser.getUid(), userid, msg);
-                }
-                else{
-                    Toast.makeText(MessageActivity.this, "Tidak bisa mengirim pesan kosong", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
         reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
 
         reference.addValueEventListener(new ValueEventListener() {
@@ -100,14 +76,5 @@ public class MessageActivity extends AppCompatActivity {
 
             }
         });
-    }
-
-    private void sendMessage(String sender, String receiver, String message){
-        HashMap<String, Object> hashMap = new HashMap<>();
-        hashMap.put("sender", sender);
-        hashMap.put("receiver", receiver);
-        hashMap.put("message", message);
-
-        reference.child("Chats").push().setValue(hashMap);
     }
 }
